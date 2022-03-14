0f3e106afb48e4a506934a9e880c49dbb074081a
Shevelev Yurii <shevelev.yu.lightit@gmail.com>
2022-03-14 21:11:32 +0200

    feat(test): Added 'hello' to main_dev.dart
    Added 'hello' to main_dev.dart

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,10 @@
 import 'package:base_project/application/application.dart';
-import 'package:flutter/material.dart';
 import 'package:base_project/config/configs/dev_config.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   await DevConfig().init();
 
   runApp(Application());
+  print('hello');
 }
